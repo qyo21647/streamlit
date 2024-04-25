115fa2b119d0919bc70a91cf2b5abcd3bc7b1fa5
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2024-04-25 12:23:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
             # Mostrar imagen de perro si se encuentra la palabra "perro"
             if "perro" in output_text.lower():
                 st.markdown("---")
-                st.image("https://placeimg.com/200/200/animals/grayscale")
+                st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Fmammals%2Ffacts%2Fdomestic-dog&psig=AOvVaw0XYdLbMdalMbYb70eEwyj4&ust=1714127016171000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCJCS__CS3YUDFQAAAAAdAAAAABAE")
                 st.markdown("---")
         else:
             st.warning('Por favor ingresa un texto antes de procesar.')
