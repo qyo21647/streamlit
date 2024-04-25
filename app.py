76b87f2e472d42c57d8bc9a24c99d5a0945ac68e
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2024-04-25 12:22:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from rich.console import Console
 
 def swap_case(text):
     return text.swapcase()
@@ -13,19 +12,15 @@ def main():
     # Botón para procesar el texto
     if st.button('Procesar texto'):
         if input_text:
-            console = Console()
             output_text = swap_case(input_text)
-            # Separar el texto en símbolos y palabras
-            words = output_text.split()
-            symbols = [char for word in words for char in word if not char.isalnum()]
-            text_without_symbols = ''.join(word for word in words if word.isalnum())
-
-            # Colorear los símbolos en rojo
-            for symbol in symbols:
-                text_without_symbols = text_without_symbols.replace(symbol, console.render(symbol, style="red"))
-
             st.write('Texto procesado:')
-            st.write(text_without_symbols)
+            st.write(output_text)
+
+            # Mostrar imagen de perro si se encuentra la palabra "perro"
+            if "perro" in output_text.lower():
+                st.markdown("---")
+                st.image("https://placeimg.com/200/200/animals/grayscale")
+                st.markdown("---")
         else:
             st.warning('Por favor ingresa un texto antes de procesar.')
 
